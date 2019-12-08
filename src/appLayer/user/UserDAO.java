40c5677e1b125d2e3c49e721376c7332823e898a
PaliLo815 <58646761+PaliLo815@users.noreply.github.com>
2019-12-08 13:04:55 +0900

    my basic implement
    1212

---
+++ src/appLayer/user/UserDAO.java
@@ -0,0 +1,83 @@
+package user;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import util.DatabaseUtil;
+
+public class UserDAO {	//Access Object
+	
+	public int login(String userID, String userPassword) {
+		String SQL = "SELECT userPassword FROM USER WHERE userID = ?";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = DatabaseUtil.getConnection();
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, userID);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				if(rs.getString(1).equals(userPassword))
+					return 1;	// password matched
+				else
+					return 0;	// not matched
+			}
+			return -1;		// no ID
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {if(conn != null) conn.close();} catch(Exception e) {e.printStackTrace();}
+			try {if(pstmt != null) pstmt.close();} catch(Exception e) {e.printStackTrace();}
+			try {if(rs != null) rs.close();} catch(Exception e) {e.printStackTrace();}
+		}
+		return -2;	// Something wrong in database
+	}
+	
+	public int registration(UserDTO user) {
+		String SQL = "INSERT INTO USER VALUE (?, ?, ?, ?)";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = DatabaseUtil.getConnection();
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, user.getUserID());
+			pstmt.setString(2, user.getUserPassword());
+			pstmt.setString(3, user.getUserName());
+			pstmt.setString(4, user.getuserClass());
+			return pstmt.executeUpdate();
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {if(conn != null) conn.close();} catch(Exception e) {e.printStackTrace();}
+			try {if(pstmt != null) pstmt.close();} catch(Exception e) {e.printStackTrace();}
+			try {if(rs != null) rs.close();} catch(Exception e) {e.printStackTrace();}
+		}
+		return -1;	// registration failed
+	}
+	
+	public boolean modify(String userID, String index, String newValue) {
+		String SQL = "UPDATE USER SET ? = ? WHERE userID = ?";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = DatabaseUtil.getConnection();
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, index);
+			pstmt.setString(2, newValue);
+			pstmt.setString(3, userID);
+			pstmt.executeUpdate();
+			return true;	// user info is changed
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {if(conn != null) conn.close();} catch(Exception e) {e.printStackTrace();}
+			try {if(pstmt != null) pstmt.close();} catch(Exception e) {e.printStackTrace();}
+			try {if(rs != null) rs.close();} catch(Exception e) {e.printStackTrace();}
+		}
+		return false;	// Something wrong in database
+	}
+}
---
+++ src/appLayer/user/UserDTO.java
@@ -0,0 +1,42 @@
+package user;
+
+public class UserDTO {	// Transfer Object
+	private String userID;
+	private String userPassword;
+	private String userName;
+	private String userClass;
+	
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getUserPassword() {
+		return userPassword;
+	}
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getuserClass() {
+		return userClass;
+	}
+	public void setuserClass(String userClass) {
+		this.userClass = userClass;
+	}
+	
+	public UserDTO() {
+	}
+	public UserDTO(String userID, String userPassword, String userName, String userClass) {
+		this.userID = userID;
+		this.userPassword = userPassword;
+		this.userName = userName;
+		this.userClass = userClass;
+	}
+}
